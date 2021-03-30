376c8777f865528aca2bbf3410c8bc1d8e8794b7
utopier <utopier2025@gmail.com>
2021-03-30 21:28:49 +0900

    ReacommendSwiper Test

--- src/Components/RecommendSwiper/RecommendSwiper.tsx
+++ src/Components/RecommendSwiper/RecommendSwiper.tsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import React from 'react';
+// import styled from 'styled-components';
+// import Slider from "react-slick";
 
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-// import 'swiper/components/scrollbar/scrollbar.scss';
-
-// install Swiper components
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
-
-import styled from 'styled-components';
-
-const SwiperWrapper = styled(Swiper)`
-  width: 100%;
-  padding: 0 20px;
-  div[class^='swiper-slide'] {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
 
 interface IRecommendSwiperProps {
   imageUrls:IImgUrl[];
@@ -33,30 +17,7 @@ interface IImgUrl {
 }
 
 const RecommendSwiper : React.FC<IRecommendSwiperProps> =  ({ imageUrls }) => {
-  const [slidesPerView, setSlidesPerView] = useState(4);
 
-  useEffect(() => {
-    setSlidesPerViewFunc();
-    window.addEventListener('resize', function () {
-      setSlidesPerViewFunc();
-    });
-  }, []);
-
-  const setSlidesPerViewFunc = () => {
-    const width = document.body.clientWidth;
-    if (width >= 900) {
-      setSlidesPerView(4);
-    } else if (760 < width && width <= 900) {
-      setSlidesPerView(3);
-    } else if (560 < width && width <= 760) {
-      setSlidesPerView(2);
-    } else if (width <= 560) {
-      setSlidesPerView(1);
-    }
-    console.log(width);
-    console.log(760 < width && width <= 960);
-    console.log(slidesPerView);
-  };
 
   return (
     <>
@@ -73,28 +34,18 @@ const RecommendSwiper : React.FC<IRecommendSwiperProps> =  ({ imageUrls }) => {
       >
         <span>추천상품</span>
       </div>
-      <SwiperWrapper
-        // spaceBetween={1}
-        slidesPerView={slidesPerView}
-        // navigation
-        scrollbar={{ draggable: true }}
-        pagination={{ clickable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide>
-          <img src={imageUrls[0].url} width="250" height="250" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imageUrls[0].url} width="250" height="250" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imageUrls[0].url} width="250" height="250" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={imageUrls[0].url} width="250" height="250" />
-        </SwiperSlide>
-      </SwiperWrapper>
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
     </>
   );
 };
