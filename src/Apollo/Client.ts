47ea4570603de938ca7e2d94e556d07d8d30f2d7
utopier <utopier2025@gmail.com>
2021-06-03 23:20:10 +0900

    httpLink header

--- src/Apollo/Client.ts
+++ src/Apollo/Client.ts
@@ -48,6 +48,10 @@ const httpLink = new HttpLink({
   //  uri: 'http://localhost:4000/graphql',
   fetchOptions:{
     mode:'no-cors'
+  },
+  headers:{
+      'Content-Type': `application/json`,
+      'Accept'      : `application/json`
   }
 })
 
